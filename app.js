c308864bc5ee96eb38920f432b654a724b0805ac
loklokyuen <loklokyuen.m@gmail.com>
2025-01-27 15:22:16 +0000

    feat: add GET /api/articles/:article_id endpoint to return an article by its id

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require("express");
 const { getEndpointDescription } = require("./controllers/api.controller");
 const { getTopics } = require("./controllers/topics.controller");
 const { handleCustomErrors, handleServerErrors } = require("./errors");
+const { getArticleById } = require("./controllers/articles.controller");
 const app = express();
 
 app.use(express.json())
 
 app.get("/api", getEndpointDescription)
 app.get("/api/topics", getTopics)
+app.get("/api/articles/:article_id", getArticleById)
 
 app.all("/*", (req, res)=>{
     res.status(404).send({ msg: "Page not found" });
