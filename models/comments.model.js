1fbd8b77581abae1453bcd785f5fb1f2ff7ed1c3
loklokyuen <loklokyuen.m@gmail.com>
2025-01-28 15:05:18 +0000

    feat: add DELETE /api/comments/:comment_id to delete comment by comment id

---
+++ models/comments.model.js
@@ -0,0 +1,11 @@
+const db = require("../db/connection")
+
+exports.removeCommentById = (comment_id)=>{
+    return db.query(`DELETE FROM comments WHERE comment_id = $1`, [comment_id])
+    .then(({rowCount})=>{
+        if (rowCount === 0){
+            console.log("reject")
+            return Promise.reject({code: 404, msg: "Comment not found"})
+        }
+    })
+}
